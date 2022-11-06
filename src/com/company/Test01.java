aa15b0dffacef3229d910fb14d69d9691c5e7e2f
Ilgar Babayev <babayev.ilqar@gmail.com>
2022-11-06 22:39:57 +0100

    update

--- src/com/company/Test01.java
+++ src/com/company/Test01.java
@@ -1,52 +1,22 @@
 package com.company;
 
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Test01 {
 
     public static void main(String[] args) throws IOException {
-        int[] tests = {1, 2};
-        System.out.println(tests.length);
+        System.out.println(factorial(100));
     }
 
-    static List<Ttt> getTests() {
-        var t01 = new Ttt(1L, false);
-        var t02 = new Ttt(2L, true);
-        var t03 = new Ttt(3L, false);
-        var t04 = new Ttt(4L, true);
-        var t05 = new Ttt(5L, false);
-        var t06 = new Ttt(6L, true);
-        var t08 = new Ttt(8L, true);
-        var t07 = new Ttt(7L, true);
-        var t10 = new Ttt(10L, false);
-        var t09 = new Ttt(9L, false);
+    private static int factorial(int num) {
+        if (num == 0) return 1;
 
-        return List.of(t01, t02, t03, t04, t05, t06, t08, t07, t10, t09);
+        return num * factorial(num - 1);
     }
 }
 
-class Ttt {
-    public Ttt(Long id, boolean isDeleted) {
-        this.id = id;
-        this.isDeleted = isDeleted;
-    }
-    Long id;
-    boolean isDeleted;
-
-    @Override
-    public String toString() {
-        return "Ttt{" +
-            "id=" + id +
-            ", isDeleted=" + isDeleted +
-            '}';
-    }
-}
 
 
 
--- src/com/company/sort/Shell.java
+++ src/com/company/sort/Shell.java
@@ -7,31 +7,30 @@ public class Shell {
 //        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 //        int[] intArray = {125, 99, 89, 66, 66, 12, 7, 4, 4, -9, -65 };
 //        int[] intArray = {-9, 99,  66, 4, 66,  -65, 125, 12, 7, 89,  4};
-        int[] intArray = {4,7,-9,12,66,-65,89,4,66, 99, 125};
 
-        int count = 0;
+//        int[] intArray = {4,7,-9,12,66,-65,89,4,66, 99, 125};
+        int[] intArray = {16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        System.out.println(Arrays.toString(intArray));
+        System.out.println("=========================");
 
         for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
-            System.out.println(Arrays.toString(intArray));
-            System.out.println("gap = " + gap);
+            System.out.println(gap);
+
             for (int i = gap; i < intArray.length; i++) {
+
                 int newElement = intArray[i];
-                System.out.println("newElement = " + newElement);
                 int j = i;
-                System.out.println("= " + intArray[j - gap]);
+
                 while (j >= gap && intArray[j - gap] > newElement) {
                     intArray[j] = intArray[j - gap];
-                    System.out.println("j = " + j);
                     j -= gap;
-                    System.out.println("j = " + j);
-                    count++;
                 }
-                count++;
+
                 intArray[j] = newElement;
             }
+            System.out.println(Arrays.toString(intArray));
         }
-
+        System.out.println("=========================");
         System.out.println(Arrays.toString(intArray));
-        System.out.println(count);
     }
 }
