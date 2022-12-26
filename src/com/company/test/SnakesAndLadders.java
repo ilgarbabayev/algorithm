e7810054163f2bb6f7c2ec8f79d9f513a0e7d90d
Ilgar Babayev <babayev.ilqar@gmail.com>
2022-12-26 16:05:31 +0100

    update

--- src/com/company/test/SnakesAndLadders.java
+++ src/com/company/test/SnakesAndLadders.java
@@ -2,14 +2,9 @@ package com.company.test;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 /*
