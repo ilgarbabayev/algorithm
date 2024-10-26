e18758293fdd0f1b1455c31995162f756cf4e68c
ibabayev <ilgar.babayev@copaco.com>
2024-10-26 11:51:34 +0200

    update

---
+++ src/com/company/FAANG/search/FindFirstLastIndex.java
@@ -0,0 +1,70 @@
+package com.company.FAANG.search;
+
+/*
+    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+    If target is not found in the array, return [-1, -1].
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+
+    Example 1:
+
+    Input: nums = [5,7,7,8,8,10], target = 8
+    Output: [3,4]
+    Example 2:
+
+    Input: nums = [5,7,7,8,8,10], target = 6
+    Output: [-1,-1]
+    Example 3:
+
+    Input: nums = [], target = 0
+    Output: [-1,-1]
+ */
+
+import java.util.Arrays;
+
+public class FindFirstLastIndex {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,7,7,8,8,10};
+        int target = 8;
+
+        System.out.println(Arrays.toString(getResult(nums, target)));
+    }
+
+    private static int[] getResult(int[] nums, int target) {
+        int[] result = {-1, -1};
+        int left = binarySearch(nums, target, true);
+        int right = binarySearch(nums, target, false);
+        result[0] = left;
+        result[1] = right;
+
+        return result;
+    }
+
+    private static int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
+        int left = 0;
+        int right = nums.length - 1;
+        int idx = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                idx = mid;
+                if (isSearchingLeft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return idx;
+    }
+}
