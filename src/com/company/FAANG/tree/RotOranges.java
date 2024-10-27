7e1bd817291156c51677be836535b825e1379871
ibabayev <ilgar.babayev@copaco.com>
2024-10-27 08:29:53 +0100

    update

--- src/com/company/FAANG/tree/RotOranges.java
+++ src/com/company/FAANG/tree/RotOranges.java
@@ -1,10 +1,18 @@
 package com.company.FAANG.tree;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+/*
+  You are given an m x n grid where each cell can have one of three values:
+
+  0 representing an empty cell,
+  1 representing a fresh orange, or
+  2 representing a rotten orange.
+  Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+  Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+ */
+
 import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class RotOranges {
