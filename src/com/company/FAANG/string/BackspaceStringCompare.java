34a5d9552cd33f7eaf5621566e36d3b42f4d976c
ibabayev <ilgar.babayev@copaco.com>
2026-02-16 21:27:54 +0100

    refactor: simplify list node string representation

--- src/com/company/FAANG/string/BackspaceStringCompare.java
+++ src/com/company/FAANG/string/BackspaceStringCompare.java
@@ -5,54 +5,54 @@ public class BackspaceStringCompare {
 /*  Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
   Note that after backspacing an empty text, the text will continue empty.*/
 
-  public static void main(String[] args) throws RuntimeException {
-    System.out.println(compareStrings("abc#d", "abzz##d"));
-    System.out.println(compareStrings("ab##", "c#d#"));
-    System.out.println(compareStrings("a#c", "b"));
-  }
-
-  public static boolean compareStrings(String S, String T) {
-
-    var p1 = S.length() - 1;
-    var p2 = T.length() - 1;
-
-    while(p1 >= 0 || p2 >= 0) {
-      if(S.charAt(p1) == '#' || T.charAt(p2) == '#') {
-        if(S.charAt(p1) == '#') {
-          var backCount = 2;
-
-          while(backCount > 0) {
-            p1--;
-            backCount--;
-
-            if(p1 >= 0 && S.charAt(p1) == '#') {
-              backCount += 2;
-            }
-          }
-        }
-
-        if(T.charAt(p2) == '#') {
-          var backCount = 2;
-
-          while(backCount > 0) {
-            p2--;
-            backCount--;
-
-            if(p2 >= 0 && T.charAt(p2) == '#') {
-              backCount += 2;
-            }
-          }
-        }
-      } else {
-        if(S.charAt(p1) != T.charAt(p2)) {
-          return false;
-        } else {
-          p1--;
-          p2--;
-        }
-      }
+    public static void main(String[] args) throws RuntimeException {
+        System.out.println(new BackspaceStringCompare().backspaceCompare("abc#d", "abzz##d"));
+        System.out.println(new BackspaceStringCompare().backspaceCompare("ab##", "c#d#"));
+        System.out.println(new BackspaceStringCompare().backspaceCompare("a#c", "b"));
     }
 
-    return true;
-  }
+    public boolean backspaceCompare(String s, String t) {
+        int p1 = s.length() - 1;
+        int p2 = t.length() - 1;
+
+        while (p1 >= 0 && p2 >= 0) {
+            p1 = getNextValidChar(s, p1);
+            p2 = getNextValidChar(t, p2);
+
+            if (p1 < 0 && p2 < 0) {
+                return true;
+            }
+
+            if (p1 < 0 || p1 < 0) {
+                return false;
+            }
+
+            if (s.charAt(p1) != t.charAt(p2)) {
+                return false;
+            }
+
+            p1--;
+            p2--;
+        }
+
+        return true;
+    }
+
+    private int getNextValidChar(String str, int end) {
+        int backspace = 0;
+
+        while (end >= 0) {
+            if (str.charAt(end) == '#') {
+                backspace++;
+            } else if (backspace > 0) {
+                backspace--;
+            } else {
+                break;
+            }
+
+            end--;
+        }
+
+        return end;
+    }
 }
