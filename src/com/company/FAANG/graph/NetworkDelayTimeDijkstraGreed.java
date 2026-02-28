344a7fab3d468a9a4992767ba5e452320c402908
ibabayev <ilgar.babayev@copaco.com>
2026-02-28 19:40:27 +0100

    refactor: rename NetworkDelayTime class to NetworkDelayTimeDijkstraGreed and enable main method output

--- src/com/company/FAANG/graph/NetworkDelayTimeDijkstraGreed.java
+++ src/com/company/FAANG/graph/NetworkDelayTimeDijkstraGreed.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class NetworkDelayTime {
+public class NetworkDelayTimeDijkstraGreed {
 
   public static void main(String[] args) {
     int n1 = 4;
@@ -45,11 +45,11 @@ public class NetworkDelayTime {
         new int[][] {{3, 5, 78}, {2, 1, 1}, {1, 3, 0}, {4, 3, 59}, {5, 3, 85}, {5, 2, 22}, {2, 4, 23}, {1, 4, 43}, {4, 5, 75}, {5, 1, 15},
             {1, 5, 91}, {4, 1, 16}, {3, 2, 98}, {3, 4, 22}, {5, 4, 31}, {1, 2, 0}, {2, 5, 4}, {4, 2, 51}, {3, 1, 36}, {2, 3, 59}};
 
-//    System.out.println(networkDelayTime(times1, n1, k1));
-//    System.out.println(networkDelayTime(times2, n2, k2));
-//    System.out.println(networkDelayTime(times3, n3, k3));
-//    System.out.println(networkDelayTime(times4, n4, k4));
-//    System.out.println(networkDelayTime(times5, n5, k5));
+    System.out.println(networkDelayTime(times1, n1, k1));
+    System.out.println(networkDelayTime(times2, n2, k2));
+    System.out.println(networkDelayTime(times3, n3, k3));
+    System.out.println(networkDelayTime(times4, n4, k4));
+    System.out.println(networkDelayTime(times5, n5, k5));
     System.out.println(networkDelayTime(times6, n6, k6));
   }
 
